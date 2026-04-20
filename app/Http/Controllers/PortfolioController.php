0b19b1c709a08896421d60c4ca8a7f0d0279002c
gazi04 <gazmendhalili2016@gmail.com>
2026-04-20 21:30:59 +0200

    :sparkles: Add PortfolioController for the home page

---
+++ app/Http/Controllers/PortfolioController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogPost;
+use App\Models\Experience;
+use App\Models\Project;
+use App\Models\SiteSetting;
+use App\Models\Skill;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class PortfolioController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Home', [
+            'person'     => SiteSetting::all()->pluck('value', 'key'),
+            'projects'   => Project::where('is_visible', true)->orderBy('id')->get(),
+            'experience' => Experience::orderBy('id')->get(),
+            'skills'     => Skill::orderBy('id')->get(),
+            'posts'      => BlogPost::where('is_published', true)->latest()->get(),
+        ]);
+    }
+}
